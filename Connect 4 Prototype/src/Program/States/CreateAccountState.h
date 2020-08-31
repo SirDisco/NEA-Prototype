db8ef90e77a57cb3b630c9db21c92c4dc33db9d0
SirDisco <josephwalker2010@hotmail.co.uk>
2020-08-31 17:52:36 +0100

    Created 'CreateAccountState' functionality.
    You can now create an account with a username and password and all files get created / updated correctly.

--- Connect 4 Prototype/src/Program/States/CreateAccountState.h
+++ Connect 4 Prototype/src/Program/States/CreateAccountState.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <fstream>
 
 #include "Program/Program.h"
+#include "Systems/Account.h"
 
 namespace NEA
 {
@@ -19,30 +22,92 @@ namespace NEA
 		{
 		}
 
+		bool CheckNameIsFree(std::string& name)
+		{
+			std::ifstream listOfUsers("res/Accounts/accounts.ls");
+
+			std::string line;
+			while (std::getline(listOfUsers, line))
+			{
+				if (name == line)
+				{
+					listOfUsers.close();
+					return false;
+				}
+			}
+
+			listOfUsers.close();
+
+			return true;
+		}
+
 		void Execute() override
 		{
 			printf("\n-------------------------------------------------\n\n");
 
-			printf("Place holder state. Nothing here for now\n\n");
+			printf("Create An Account\n");
+			printf("	- At any point type 'QUIT' to exit\n\n");
 
-			printf("Please select an option\n\n");
-			printf("1) Back\n\n");
 
-			int input = -1;
-			std::cin >> input;
+			std::string userName;
+			std::string password;
 
-			while (input < 1 || input > 1)
+			bool available = false;
+
+			// Get Username from user
+
+			while (!available)
 			{
-				printf("Invalid Choice\n");
-				std::cin >> input;
+				printf("Please enter username: ");
+
+				std::cin.ignore(1, '\n');
+
+				std::getline(std::cin, userName);
+
+				if (userName._Equal("QUIT"))
+				{
+					Program::s_Instance->PopState();
+					return;
+				}
+				else
+				{
+					// Check username doesn't already exist
+					available = CheckNameIsFree(userName);
+				}
+
+				if (!available)
+					printf("\n	! Invalid Name !\n\n");
 			}
 
-			switch (input)
+			// Ask user for password
+
+			printf("Please enter password: ");
+
+			std::getline(std::cin, password);
+
+			if (password._Equal("QUIT"))
 			{
-			case 1:
 				Program::s_Instance->PopState();
-				break;
+				return;
 			}
+
+			// Create new account and save to file
+
+			Account newAccount(userName, password);
+			newAccount.SaveToFile();
+
+			// Add new account to list of accounts
+
+			std::ofstream listOfUsers("res/Accounts/accounts.ls", std::ios::ate);
+
+			listOfUsers << userName;
+
+			listOfUsers.close();
+
+			printf("\nAccount created successfully\n");
+			
+			Program::s_Instance->PopState();
+
 		}
 
 	};
