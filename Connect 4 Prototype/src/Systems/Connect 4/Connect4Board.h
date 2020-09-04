4923aa541913be3cad1d69727fa8902a46992bca
SirDisco <josephwalker2010@hotmail.co.uk>
2020-09-04 12:26:06 +0100

    Added function definitions for all board functions so that no errors occur in other parts of code

--- Connect 4 Prototype/src/Systems/Connect 4/Connect4Board.h
+++ Connect 4 Prototype/src/Systems/Connect 4/Connect4Board.h
@@ -1,1 +1,46 @@
 #pragma once
+
+namespace NEA
+{
+	class Connect4Board
+	{
+	public:
+
+		Connect4Board()
+		{
+
+		}
+
+		virtual ~Connect4Board()
+		{
+
+		}
+
+	public:
+
+		void PlaceCounter(int column, char character)
+		{
+
+		}
+
+		bool GameOver()
+		{
+
+		}
+
+		bool GameWon()
+		{
+
+		}
+
+		bool GameDrawn()
+		{
+
+		}
+
+	private:
+
+
+
+	};
+}
